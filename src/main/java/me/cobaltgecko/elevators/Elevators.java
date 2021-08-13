a4d16295eda953e277032955183b942592a4d67a
bsimpleman <blakeman90911@gmail.com>
2021-08-13 09:26:11 -0500

    Initial commit

---
+++ src/main/java/me/cobaltgecko/elevators/Elevators.java
@@ -0,0 +1,17 @@
+package me.cobaltgecko.elevators;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Elevators extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
